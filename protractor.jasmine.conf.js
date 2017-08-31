1e2d6c260768b24bc10324568ebd6491ae9e4cf0
marcos-x86 <marcos.x86@outlook.com>
2017-08-31 15:25:45 -0400

    Initial project

---
+++ protractor.jasmine.conf.js
@@ -0,0 +1,31 @@
+exports.config = {
+	seleniumAddress: "http://localhost:4444/wd/hub",
+
+	capabilities: {
+		"browserName": "chrome",
+		"chromeOptions": {
+			"args": ["--disable-extensions", "--start-maximized"]
+		}
+	},
+
+	specs: ["./test/e2e/specs/login.spec.js"],
+
+	framework: "jasmine",
+
+	jasmineNodeOpts: {
+		showColors: true,
+		defaultTimeoutInterval: 3000000
+	},
+
+	onPrepare: function() {
+		require("jasmine-co").install();
+		global.EC = protractor.ExpectedConditions;
+	},
+
+	params : {
+	    username: "",
+		password: "",
+		token: "",
+		baseUrl: "https://www.pivotaltracker.com/"
+	}
+};
---
+++ test/e2e/specs/login.spec.js
@@ -0,0 +1,17 @@
+"use strict";
+
+describe("Login", function () {
+	const loginPage = require("../pages/login.page");
+	const dashboardPage = require("../pages/dashboard.page");
+
+	beforeEach(() => {
+		browser.waitForAngularEnabled(false);
+		browser.get(browser.params.baseUrl+"signin");
+	});
+
+	it("Login with valid credentials", function*() {
+		yield loginPage.setName(browser.params.username);
+		yield loginPage.setPassword(browser.params.password);
+		expect(yield dashboardPage.getPageTitle()).toBe("Dashboard - Pivotal Tracker");
+	});
+});
